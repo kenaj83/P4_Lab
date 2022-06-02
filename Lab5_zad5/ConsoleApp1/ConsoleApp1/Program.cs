2914b7dfe02cc50ced7a167fdd81a7dfab95b6d8
kenaj83 <kenaj83@gmail.com>
2022-06-02 06:27:37 +0200

    dodano instrukcje case

--- Lab5_zad5/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lab5_zad5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -64,75 +64,159 @@ while (wybór != 4)
     Console.WriteLine("***********************************************");
     Console.WriteLine();
 
+    Console.Write($"Wybierz co chcesz zrobić: ");
     wybór = Int32.Parse(Console.ReadLine());
+    Console.WriteLine();
+    
 
-    if (wybór == 1)
+    switch (wybór)
     {
-        Console.WriteLine($"Podaj nazwisko autora");
+        case 1:
 
-        string nazwisko = Console.ReadLine();
+            Console.Write($"Podaj nazwisko autora: ");
+            string nazwisko = Console.ReadLine();
 
-        var query = context.Autor
-                            .Where(n => n.Nazwisko == nazwisko)
-                            .Include(s => s.SpisKsiazek)
-                            .ToList();
-
-        foreach (var item in query)
-        {
-            Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
-
-            foreach (var itemKsiazki in item.SpisKsiazek)
+            var query = context.Autor
+                                .Where(n => n.Nazwisko == nazwisko)
+                                .Include(s => s.SpisKsiazek)
+                                .ToList();
+            Console.WriteLine();
+            foreach (var item in query)
             {
-                Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+                Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
+
+                foreach (var itemKsiazki in item.SpisKsiazek)
+                {
+                    Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+                }
             }
-        }
-    }
-    if (wybór == 2)
-    {
-        Console.WriteLine($"Podaj imię autora");
+            break;
 
-        string imię = Console.ReadLine();
-
-        var query = context.Autor
-                            .Where(i => i.Imie == imię)
-                            .Include(s => s.SpisKsiazek)
-                            .ToList();
-
-        foreach (var item in query)
-        {
-            Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
-
-            foreach (var itemKsiazki in item.SpisKsiazek)
+        case 2:
             {
-                Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+                Console.Write($"Podaj imię autora: ");
+              
+
+                string imię = Console.ReadLine();
+
+                var query2 = context.Autor
+                                    .Where(i => i.Imie == imię)
+                                    .Include(s => s.SpisKsiazek)
+                                    .ToList();
+
+                Console.WriteLine();
+                foreach (var item in query2)
+                {
+                    Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
+
+                    foreach (var itemKsiazki in item.SpisKsiazek)
+                    {
+                        Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+                    }
+                }
+                break;
             }
-        }
-    }
-    else if (wybór == 3)
-    {
-        Console.WriteLine($"Podaj tytuł książki");
-        string tytul = Console.ReadLine();
+            case 3:
+            {
 
-        var query = context.Ksiazka
-                            .Where(t => t.Tytul == tytul)
-                            .Include(a => a.Autor)
-                            .ToList();
+                Console.Write($"Podaj tytuł książki: ");
+                string tytul = Console.ReadLine();
+                Console.WriteLine();
 
-        foreach (var itemKsiazki in query)
-        {
-            Console.WriteLine($"Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}, Imie autora: {itemKsiazki.Autor.Imie}, Nazwisko autora: {itemKsiazki.Autor.Nazwisko}");
-        }
+                var query3 = context.Ksiazka
+                                    .Where(t => t.Tytul == tytul)
+                                    .Include(a => a.Autor)
+                                    .ToList();
 
-    }
-    else if (wybór == 4)
-    {
-        Console.WriteLine("Koniec");
+                foreach (var itemKsiazki in query3)
+                {
+                    Console.WriteLine($"Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}, Imie autora: {itemKsiazki.Autor.Imie}, Nazwisko autora: {itemKsiazki.Autor.Nazwisko}");
+                }
 
-    }
-    else
-    {
-        Console.WriteLine("Nie ma takiego numeru");
+                break;
+            }
+        case 4:
+            {
+                
+                Console.WriteLine("Koniec");
+                Console.WriteLine();
+
+                break;
+            }
+
+        default:
+            Console.WriteLine($"Nie ma takiego numeru");
+            break;
     }
 }
+
+    //if (wybór == 1)
+    //{
+    //    Console.WriteLine($"Podaj nazwisko autora");
+
+    //    string nazwisko = Console.ReadLine();
+
+    //    var query = context.Autor
+    //                        .Where(n => n.Nazwisko == nazwisko)
+    //                        .Include(s => s.SpisKsiazek)
+    //                        .ToList();
+
+    //    foreach (var item in query)
+    //    {
+    //        Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
+
+    //        foreach (var itemKsiazki in item.SpisKsiazek)
+    //        {
+    //            Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+    //        }
+    //    }
+    //}
+    //if (wybór == 2)
+    //{
+    //    Console.WriteLine($"Podaj imię autora");
+
+    //    string imię = Console.ReadLine();
+
+    //    var query = context.Autor
+    //                        .Where(i => i.Imie == imię)
+    //                        .Include(s => s.SpisKsiazek)
+    //                        .ToList();
+
+    //    foreach (var item in query)
+    //    {
+    //        Console.WriteLine($"Imie: {item.Imie}, Nazwisko: {item.Nazwisko} ");
+
+    //        foreach (var itemKsiazki in item.SpisKsiazek)
+    //        {
+    //            Console.WriteLine($"\t Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}");
+    //        }
+    //    }
+    //}
+    //else if (wybór == 3)
+    //{
+    //    Console.WriteLine($"Podaj tytuł książki");
+    //    string tytul = Console.ReadLine();
+
+    //    var query = context.Ksiazka
+    //                        .Where(t => t.Tytul == tytul)
+    //                        .Include(a => a.Autor)
+    //                        .ToList();
+
+    //    foreach (var itemKsiazki in query)
+    //    {
+    //        Console.WriteLine($"Tytuł: {itemKsiazki.Tytul}, Rok wydania: {itemKsiazki.RokWydania}, Imie autora: {itemKsiazki.Autor.Imie}, Nazwisko autora: {itemKsiazki.Autor.Nazwisko}");
+    //    }
+
+    //}
+    //else if (wybór == 4)
+    //{
+    //    Console.WriteLine("Koniec");
+
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Nie ma takiego numeru");
+    //}
+
 
 
