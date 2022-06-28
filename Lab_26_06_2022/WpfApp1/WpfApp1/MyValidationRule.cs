2eb48dafd28fe344065769a2d47d989c1354fad0
kenaj83 <kenaj83@gmail.com>
2022-06-28 18:41:56 +0200

    nowości

---
+++ Lab_26_06_2022/WpfApp1/WpfApp1/MyValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    internal class MyValidationRule : ValidationRule
+    {
+
+
+        public int minLenght;
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
